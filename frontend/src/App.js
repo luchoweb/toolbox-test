b425232ce2621fad94f0cbef8a4b281111636c48
Lucho Web <luchoweb.dev@gmail.com>
2022-09-27 13:42:18 -0500

    fecth data progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
 import './App.css';
 
 function App() {
+  const [fileData, setFileData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch('http://localhost:5000/files/data');
+        const dataJson = await data.json();
+        console.log(dataJson);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>Text</th>
+            <th>Number</th>
+            <th>Hex</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!loading ? 
+            fileData.length ? (
+              fileData.map(item => (
+                <tr key={`name-${item.file}`}>
+                  <td>{ item.file }</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={4}>
+                  No data to show. Please refresh the page.
+                </td>
+              </tr>
+            )
+          : (
+            <tr>
+              <td colSpan={4}>
+                Loading data, please wait...
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 }
