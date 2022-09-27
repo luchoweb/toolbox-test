0531c82d9296f45b2bfa6e1a4f5ebc887979fe37
Lucho Web <luchoweb.dev@gmail.com>
2022-09-26 20:59:12 -0500

    API (#1)

---
+++ api/controllers/files.controller.js
@@ -0,0 +1,69 @@
+const fetch = require('node-fetch');
+
+// Create a method for getting all files
+const getAllFiles = async (req, res) => {
+  try {
+    // Set up external api args
+    const externalApiUri = 'https://echo-serv.tbxnet.com/v1';
+    const externalApiOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer aSuperSecretKey'
+      }
+    };
+
+    // Getting all files names
+    const getFilesName = await fetch(`${externalApiUri}/secret/files`, externalApiOptions);
+    // Response to json
+    const jsonFilesName = await getFilesName.json();
+
+    // Files filtered by default
+    const filesFiltered = [];
+
+    // Create a loop to go through and download all the files
+    for ( let i = 0; i < jsonFilesName.files.length; i++) {
+      // Current file name
+      const file = jsonFilesName.files[i];
+
+      // Get current file by name
+      const getFileContent = await fetch(`${externalApiUri}/secret/file/${file}`, externalApiOptions);
+      // Convert response to text
+      const rawFilesContent = await getFileContent.text();
+    
+      // convert each file content to an array
+      const filesContentArr = rawFilesContent.split('\n');
+      
+      // Filter each file content and ignore errors or incomplete
+      const filesContentFiltered = filesContentArr.filter(fileContent => fileContent.indexOf('code') === -1 && fileContent.indexOf(file) > -1 && fileContent.split(',').length === 4);
+
+      // Validate if filesContentFiltered isn't empty
+      if ( filesContentFiltered.length > 0) {
+        // Map filesContentFiltered to get the lines
+        const lines = filesContentFiltered.map(line => {
+          // Convert lines to an array
+          const lineArr = line.split(',');
+
+          // Return each line object
+          return {
+            text: lineArr[1],
+            number: lineArr[2],
+            hex: lineArr[3]
+          }
+        });
+    
+        // Add to filesFiltered each new object for valid file content
+        filesFiltered.push({ file, lines });
+      }
+    }
+
+    // Send to endpoint
+    res.send(filesFiltered);
+  } catch (error) {
+    // Send error details
+    res.status(500).send(error);
+  }
+}
+
+module.exports = {
+  getAllFiles
+}
