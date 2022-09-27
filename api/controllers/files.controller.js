0413150f78ec290b7a32b31e092d4b27fc9f497f
Lucho Web <luchoweb.dev@gmail.com>
2022-09-27 07:58:56 -0500

    code improvements

--- api/controllers/files.controller.js
+++ api/controllers/files.controller.js
@@ -13,67 +13,34 @@ const externalApiOptions = {
 
 // Create a method for getting all files
 const getAllFiles = async (req, res) => {
-  try {
-    if ( req.query?.fileName ) {
-      const response = await getFileByName(req.query.fileName);
-      res.send(response);
-    } else {
-      // Getting all files names
-      const getFilesName = await fetch(`${externalApiUri}/secret/files`, externalApiOptions);
-      // Response to json
-      const jsonFilesName = await getFilesName.json();
+  // If fileName query param exist in the url
+  if ( req.query?.fileName ) {
+    // Get each file formated
+    const response = await getFileFormatted(req.query.fileName);
+    res.send(response);
+  } else {
+    // Getting all files names
+    const getFilesName = await fetch(`${externalApiUri}/secret/files`, externalApiOptions);
+    // Response to json
+    const jsonFilesName = await getFilesName.json();
+    // Array to save each file formated
+    const filesFiltered = [];
 
-      // Files filtered by default
-      const filesFiltered = [];
-
-      // Create a loop to go through and download all the files
-      for ( let i = 0; i < jsonFilesName.files.length; i++) {
-        // Current file name
-        const file = jsonFilesName.files[i];
-
-        // Get current file by name
-        const getFileContent = await fetch(`${externalApiUri}/secret/file/${file}`, externalApiOptions);
-        // Convert response to text
-        const rawFilesContent = await getFileContent.text();
-      
-        // convert each file content to an array
-        const filesContentArr = rawFilesContent.split('\n');
-        
-        // Filter each file content and ignore errors or incomplete
-        const filesContentFiltered = filesContentArr.filter(fileContent => fileContent.indexOf('code') === -1 && fileContent.indexOf(file) > -1 && fileContent.split(',').length === 4);
-
-        // Validate if filesContentFiltered isn't empty
-        if ( filesContentFiltered.length > 0) {
-          // Map filesContentFiltered to get the lines
-          const lines = filesContentFiltered.map(line => {
-            // Convert lines to an array
-            const lineArr = line.split(',');
-
-            // Return each line object
-            return {
-              text: lineArr[1],
-              number: lineArr[2],
-              hex: lineArr[3]
-            }
-          });
-      
-          // Add to filesFiltered each new object for valid file content
-          filesFiltered.push({ file, lines });
-        }
-      }
-
-      // Send to endpoint
-      res.send(filesFiltered);
+    for ( let i = 0; i < jsonFilesName.files.length; i++) {
+      // Current file name
+      const file = jsonFilesName.files[i];
+      // Get each file formated
+      const fileFormated = await getFileFormatted(file);
+      if ( fileFormated ) filesFiltered.push(fileFormated);
     }
-  } catch (error) {
-    // Send error details
-    res.status(500).send(error);
+
+    res.send(filesFiltered);
   }
 }
 
-const getFileByName = async (file) => {
+const getFileFormatted = async (file) => {
   try {
-    const filesFiltered = [];
+    let filesFiltered;
 
     // Get current file by name
     const getFileContent = await fetch(`${externalApiUri}/secret/file/${file}`, externalApiOptions);
@@ -102,7 +69,7 @@ const getFileByName = async (file) => {
       });
   
       // Add to filesFiltered each new object for valid file content
-      filesFiltered.push({ file, lines });
+      filesFiltered = { file, lines };
     }
 
     // Send to endpoint
@@ -131,6 +98,5 @@ const getAllFilesNames = async (req, res) => {
 
 module.exports = {
   getAllFiles,
-  getAllFilesNames,
-  getFileByName
+  getAllFilesNames
 }
