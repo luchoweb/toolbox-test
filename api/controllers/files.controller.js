2ea6e6bb650c3f956eaede823625fce544cf2073
Lucho Web <luchoweb.dev@gmail.com>
2022-09-28 10:42:15 -0500

    Added comments to better understand the code

--- api/controllers/files.controller.js
+++ api/controllers/files.controller.js
@@ -11,55 +11,65 @@ const externalApiOptions = {
   }
 };
 
-// Create a method for getting all files
+// Function to get one or all files
 const getAllFiles = async (req, res) => {
   // If fileName query param exist in the url
+  // Get that file formatted only
   if ( req.query?.fileName ) {
-    // Get each file formated
+    // Get the file formatted
     const response = await getFileFormatted(req.query.fileName);
+    console.log(response)
+
+    // Send to endpoint the file or 404 not found error
     res.status(response ? 200 : 404).send(
       response || { error: `File doesn't exist` }
     );
   } else {
-    // Getting all files names
+    // If fileName query param doesn't exist in the url
+    // Get all formatted files
     const getFilesName = await fetch(`${externalApiUri}/secret/files`, externalApiOptions);
-    // Response to json
     const jsonFilesName = await getFilesName.json();
-    // Array to save each file formated
+
+    // Array to save each formatted file
     const filesFiltered = [];
 
-    for ( let i = 0; i < jsonFilesName.files.length; i++) {
-      // Current file name
-      const file = jsonFilesName.files[i];
-      // Get each file formated
-      const fileFormated = await getFileFormatted(file);
-      if ( fileFormated ) filesFiltered.push(fileFormated);
+    if ( jsonFilesName?.files?.length ) {
+      // Get each filename to get the file content
+      for ( let i = 0; i < jsonFilesName?.files?.length; i++) {
+        // Current file name
+        const file = jsonFilesName.files[i];
+        // Get each formatted file
+        const fileFormated = await getFileFormatted(file);
+        if ( fileFormated ) filesFiltered.push(fileFormated);
+      }
     }
 
+    // Send to endpoint the files
     res.send(filesFiltered);
   }
 }
 
+// Function to get each file formatted
 const getFileFormatted = async (file) => {
   try {
-    let filesFiltered;
+    let formattedFile;
 
     // Get current file by name
     const getFileContent = await fetch(`${externalApiUri}/secret/file/${file}`, externalApiOptions);
     // Convert response to text
-    const rawFilesContent = await getFileContent.text();
+    const rawFileContent = await getFileContent.text();
   
-    // convert each file content to an array
-    const filesContentArr = rawFilesContent.split('\n');
+    // Separate each line from the file content
+    const fileContentArr = rawFileContent.split('\n');
     
-    // Filter each file content and ignore errors or incomplete
-    const filesContentFiltered = filesContentArr.filter(fileContent => fileContent.indexOf('code') === -1 && fileContent.indexOf(file) > -1 && fileContent.split(',').length === 4);
+    // Filter each line of file content and ignore errors or incompletes
+    const fileContentFiltered = fileContentArr.filter(fileContent => fileContent.indexOf('code') === -1 && fileContent.indexOf(file) > -1 && fileContent.split(',').length === 4);
 
     // Validate if filesContentFiltered isn't empty
-    if ( filesContentFiltered.length > 0) {
-      // Map filesContentFiltered to get the lines
-      const lines = filesContentFiltered.map(line => {
-        // Convert lines to an array
+    if ( fileContentFiltered.length ) {
+      // Map fileContentFiltered to get the lines of the file
+      const lines = fileContentFiltered.map(line => {
+        // Convert the lines to an array
         const lineArr = line.split(',');
 
         // Return each line object
@@ -70,27 +80,26 @@ const getFileFormatted = async (file) => {
         }
       });
   
-      // Add to filesFiltered each new object for valid file content
-      filesFiltered = { file, lines };
+      // Add formatted info
+      formattedFile = { file, lines };
     }
 
-    // Send to endpoint
-    return filesFiltered;
+    // Send to endpoint the formatted file
+    return formattedFile;
   } catch (error) {
     // Send error details
     return error;
   }
 }
 
-// Create a method for getting all files names
+// Function to get all file names
 const getAllFilesNames = async (req, res) => {
   try {
-    // Getting all files names
+    // Getting all file names
     const getFilesName = await fetch(`${externalApiUri}/secret/files`, externalApiOptions);
-    // Response to json
     const jsonFilesName = await getFilesName.json();
 
-    // Send to endpoint
+    // Send to endpoint the file names
     res.send(jsonFilesName);
   } catch (error) {
     // Send error details
